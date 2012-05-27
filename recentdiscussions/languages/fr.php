028e6c5949197f8f66ccce8567582750eb744fe1
Emmanuel <emanwebdev@gmail.com>
2012-05-27 17:39:17 +0200

    French language pack

---
+++ recentdiscussions/languages/fr.php
@@ -0,0 +1,9 @@
+<?php
+
+$french = array(
+  'recentdiscussions:title' => "Discussions récentes",
+  'recentdiscussions:widget:description' => "Discussions récentes",
+  'recentdiscussions:widget:numberdiscussions' => "Nombre de sujets des dernières discussions à afficher",
+);
+
+add_translation("fr",$french);
